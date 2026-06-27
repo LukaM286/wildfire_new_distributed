0266ee99bd5acddeb95c2c4abf0043e3e675ae2e
LukaM286 <mismasluka@gmail.com>
2026-06-27 11:31:39 +0200

    init distributed version (based on sequential)

---
+++ src/wildfire/Main.java
@@ -0,0 +1,20 @@
+package wildfire;
+
+public class Main {
+    public static void main(String[] args) {
+        SimConfig config = ConfigReader.readConfig("instructions.txt");
+        System.out.println("Config loaded: " + config);
+
+        WildfireSimulation sim = new WildfireSimulation(config);
+        sim.generateForest();
+        sim.igniteRandomTiles();
+
+        long startTime = System.currentTimeMillis();
+        sim.run(null);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Simulation finished!");
+        System.out.println("Total ticks: " + sim.getTick());
+        System.out.println("Time: " + (endTime - startTime) + " ms");
+    }
+}
